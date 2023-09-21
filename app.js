fa3666f603374afd40bed45268d337c522f9d3cb
Anhkiet2401 <kietb1910537@student.ctu.edu.vn>
2023-09-22 03:03:24 +0700

    Cài đặt xử lý lỗi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -9,8 +10,25 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
-     res.json({ message: "Welcome to contact book application. "});   
-    
+    res.json({ message: "Welcome to contact book application. "});   
+   
 });
+//xử lý phản hồi 404
+app.use((req, res, next) => {
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Không tìm thấy tài nguyên"));
+    });
+
+// xác định phần mềm trung gian xử lý lỗi cuối cùng, sau các lệnh gọi app.use() và định tuyến khác
+app.use((err, req, res, next) => {
+    // Middleware xử lý lỗi tập trung.
+    // Trong các đoạn code xử lý ở các route, gọi next(error)
+    // sẽ chuyển về middleware xử lý lỗi này
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Lỗi máy chủ nội bộ",
+    });   
+}); 
+
 
 module.exports = app;
